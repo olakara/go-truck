53d554c7401b8e97c1f3a219814667a8beae74b4
Abdel Raoof <olakara@gmail.com>
2025-04-16 17:25:16 +0400

    capstone exercise setup

---
+++ capstone/truck_manager.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+)
+
+var ErrTruckNotFound = errors.New("truck not found")
+
+type FleetManager interface {
+	AddTruck(id string, cargo int) error
+	GetTruck(id string) (*Truck, error)
+	RemoveTruck(id string) error
+	UpdateTruckCargo(id string, cargo int) error
+}
+
+type Truck struct {
+	ID    string
+	Cargo int
+}
+
+type truckManager struct {
+	trucks map[string]*Truck
+}
+
+func NewTruckManager() truckManager {
+	return truckManager{
+		trucks: make(map[string]*Truck),
+	}
+}
